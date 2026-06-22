ad8e3660f9ebc855725a54fabef34284d0047611
向北丶 <jay1845@foxmail.com>
2026-06-22 16:11:11 +0800

    feat: 登录和首页调整

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,9 @@ import { Flex, Button, Checkbox, Form, Input, message, Select } from 'antd';
 import { Navigate, useNavigate } from "react-router";
 import type { FormProps } from 'antd';
 
+import "./index.less";
+
+
 import { storage } from "@/utils/storage";
 import { reqLogin } from '@/services/auth';
 
@@ -66,62 +69,69 @@ export default function Login() {
   };
 
   return (
-    <Flex gap="medium" align="center" justify="center" vertical className="w-full h-screen bg-gray-100">
-      <h1>登录页面</h1>
-      <Select
-        defaultValue={{ value: 'user1', label: 'user1' }}
-        style={{ width: 120 }}
-        labelInValue
-        onChange={handleChange}
-        options={[
-          { value: 'user1', label: 'user1' },
-          { value: 'admin1', label: 'admin1' },
-          { value: 'guest1', label: 'guest1' },
-        ]}
-      />
-      <div className="bg-white p-8 rounded-lg shadow-lg">
 
-        <Form
-          form={form}
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={initialValues}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item<FieldType>
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <Input />
-          </Form.Item>
+    <div className='login-container'>
+      <div className="login-box">
+        <Flex gap="medium" align="center" justify="center" vertical className="w-full h-screen">
+          <h1>登录页面</h1>
+          <Select
+            defaultValue={{ value: 'user1', label: 'user1' }}
+            style={{ width: 120 }}
+            labelInValue
+            onChange={handleChange}
+            options={[
+              { value: 'user1', label: 'user1' },
+              { value: 'admin1', label: 'admin1' },
+              { value: 'guest1', label: 'guest1' },
+            ]}
+          />
+          <div className="bg-white p-8 rounded-lg shadow-lg">
 
-          <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form
+              form={form}
+              name="basic"
+              labelCol={{ span: 8 }}
+              wrapperCol={{ span: 16 }}
+              style={{ maxWidth: 600 }}
+              initialValues={initialValues}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item<FieldType>
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+              >
+                <Input />
+              </Form.Item>
 
-          <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+              <Form.Item<FieldType>
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+              >
+                <Input.Password />
+              </Form.Item>
 
-          <Form.Item label={null}>
-            <Button type="primary" htmlType="submit" onClick={handleLogin}>
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
+              <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item label={null}>
+                <Button type="primary" htmlType="submit" onClick={handleLogin}>
+                  登录
+                </Button>
+              </Form.Item>
+            </Form>
 
 
+          </div>
+          {contextHolder}
+        </Flex>
       </div>
-      {contextHolder}
-    </Flex>
+
+    </div>
+
   );
 }
